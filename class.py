14a22a1656ad8be3ccf85f8794aa54b37aa2777e
Okemwa Brian <114869400+okemwabrian@users.noreply.github.com>
2024-09-21 15:49:00 +0300

    Add files via upload
    class assignments

---
+++ class.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
+    def introduce(self):
+        print(f"Hello, my name is {self.name}. I am {self.age} years old, and I am {self.gender}.")
+        
+
+person1 = Person("Okemwa Brian", 22, "Male")
+
+
+person1.introduce()
